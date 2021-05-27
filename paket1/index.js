5a6088ee6600ca803d4c2c3c71f89c7e2cf5dad2
Enes Fidan <enesfidan27@gmail.com>
2021-05-27 11:25:05 +0300

    First Commit

---
+++ paket1/index.js
@@ -0,0 +1,4 @@
+const is = require("simple-is");
+console.log(is.number(23));
+
+console.log("bu 0")
---
+++ paket2/index.js
@@ -0,0 +1,5 @@
+const is = require("simple-is");
+
+console.log(is.number(27));
+
+console.log("Bu 1 soygundur")
